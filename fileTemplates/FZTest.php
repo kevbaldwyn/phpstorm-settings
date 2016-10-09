7f6272f2919188610bb73e6f414feced32d9e7d3
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2016-10-09 09:02:07 +0100

    PS-2016.2.1 <Kev@kevinbadwynsmbp.home Create baseRefactoring.xml, PHP Class.php, ide.general.xml, monokai.icls, project.default.xml, diff.xml, git.xml, other.xml, file.template.settings.xml, PHP Field Doc Comment.php, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, javaeeExternalResources.xml, code.style.schemes.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, PHP Trait.php, databaseDrivers.xml, Default.xml, PHP Overridden Method Body.php, VCL.xml, PHP File Header.php, laf.xml, Default _1_.xml, web-browsers.xml, FZTest.php, PHP Interface.php, Monokai Edited.icls, databaseSettings.xml, keymap.xml, editor.xml, usageView.xml, applicationLibraries.xml, Monokai edited - 2.icls, composer.json, webServers.xml, debugger.xml, Modified.xml

---
+++ fileTemplates/FZTest.php
@@ -0,0 +1,13 @@
+<?php#if (${NAMESPACE}) namespace ${NAMESPACE}; #end
+
+use Mockery as m;
+use Tests\App\FZMockTestCase;
+
+class ${NAME} extends FZMockTestCase {
+    
+    public function test()
+    {
+        
+    }
+    
+}
